437e8bd1a8a108642895535aa52cd78b960f15f7
Max <macsimbodnar@gmail.com>
2017-04-10 21:05:18 +0200

    Creato il frame dei dati e delle info

--- resources/views/layouts/gameframe.blade.php
+++ resources/views/layouts/gameframe.blade.php
@@ -2,8 +2,16 @@
 
 @section('content')
     <div class="container">
-        <div class="game-frame">
-            @yield('game-frame')
+        <div class="row">
+            <div class="game-frame col-8">
+                @yield('game-frame')
+            </div>
+            <div class="data-frame col-4">
+                @yield('data-frame')
+            </div>
+            <div class="info-frame col-12">
+                @yield('info-frame')
+            </div>
         </div>
     </div>
 @endsection

c5c49873c5a32182f722f9cfeb85b9b654f24430
Max <macsimbodnar@gmail.com>
2018-02-07 15:20:20 +0100

    Cambiato il bad-dragon con uselesswebsite.
    Bad dragon era un po troppo forse se voglio trovare lavoro.

--- resources/views/components/big-fat-button.blade.php
+++ resources/views/components/big-fat-button.blade.php
@@ -2,7 +2,7 @@
     {{--<script src="{{ asset('js/util/nasty-button.js') }}"></script>--}}
 @endpush
 <div class="big-fat-button-container">
-    <a id="big-fat-button-a" href="http://bad-dragon.com" target="_blank">
+    <a id="big-fat-button-a" href="http://www.theuselessweb.com/" target="_blank">
         <div id="big-fat-button" class="big-fat-button"></div>
     </a>
 </div>
